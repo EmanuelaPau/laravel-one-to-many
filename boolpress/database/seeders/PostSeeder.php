71067669c79598aec9bcf6a791527d8f27b42f1a
EmanuelaPau <uapame@gmail.com>
2023-08-25 17:46:31 +0200

    Add type id to post seeder

--- boolpress/database/seeders/PostSeeder.php
+++ boolpress/database/seeders/PostSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Admin\Post;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -11,11 +12,14 @@ class PostSeeder extends Seeder
 {
     public function run(Faker $faker): void
     {
+        $types = Type::all();
+
 
         //
         for ($i = 0; $i < 100; $i++) {
             $newPost = new Post();
             $newPost->title = ucfirst($faker->unique()->sentence());
+            $newPost->type_id = $faker->randomElements($types);
             $newPost->content = $faker->paragraph(8, true);
             $newPost->slug = $faker->slug();
             $newPost->image = $faker->imageUrl(640, 480, 'Post', true);
